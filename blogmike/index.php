f3aeb7a73222d4df67942f1d2fc2f43c219d3c6c
michaelmnolla <michael_mn153@hotmail.com>
2017-08-24 16:33:20 +0200

    blog de michael

---
+++ blogmike/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no,inicial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Blog</title>
+</head>
+<body>
+	<p>hola</p>
+</body>
+</html>
---
+++ blogmike/views/index.view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Blog</title>
+</head>
+<body>
+	<h1 id="titulo">Bienvenido a mi Blog</h1>
+	<img src="https://cdn.pixabay.com/photo/2014/05/02/21/49/home-office-336373_960_720.jpg" name="imagen" class="img1">
+	
+	
+</body>
+</html>
